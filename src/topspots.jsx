681542618a3374699ec80ff8bb129bed63cf4d90
BrianC <seahorse8789@gmail.com>
2018-09-14 12:09:54 -0700

    this is my top spots project

---
+++ src/topspots.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default props => (
+
+    <div className='well'>
+    <h4>{props.name}</h4>
+    <p>{props.description}</p>
+    <a className="btn btn-primary" href={'https://maps.google.com/?q=' + props.location} role="button">Click Here To Go To Google</a>
+</div>
+
+);
